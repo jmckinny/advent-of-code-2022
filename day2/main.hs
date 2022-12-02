d326f83ca3f5c028faee698c22564d4e163b56fc
jmckinny <justinmckinney.md@gmail.com>
2022-12-02 13:54:10 -0500

    day2 part 2

--- day2/main.hs
+++ day2/main.hs
@@ -9,7 +9,20 @@ data Outcome = Win | Loss | Draw
 
 type Score = Int
 
-
+chooseGesture :: (Gesture,Outcome) -> Gesture
+chooseGesture (opponent,goal) = case opponent of 
+    Rock -> case goal of 
+            Win -> Paper
+            Loss -> Scissors 
+            Draw -> Rock
+    Paper -> case goal of 
+            Win -> Scissors
+            Loss -> Rock
+            Draw -> Paper
+    Scissors -> case goal of 
+            Win -> Rock
+            Loss -> Paper
+            Draw -> Scissors
 
 getOutcome :: (Gesture,Gesture) -> Outcome
 getOutcome (opponent,player) = case opponent of 
@@ -37,10 +50,21 @@ computeScorePart1 (opponent,player) = gestureScore + outcomeScore
     where gestureScore = gestureToScore player
           outcomeScore = outcomeToScore $ getOutcome (opponent,player)
 
+
+computeScorePart2 :: (Gesture,Outcome) -> Score 
+computeScorePart2 (opponent,player) = gestureScore + outcomeScore
+    where choice = chooseGesture (opponent,player)
+          gestureScore = gestureToScore choice
+          outcomeScore = outcomeToScore $ getOutcome (opponent,choice)
+
+
 parseLine :: String -> (Gesture,Gesture)
 parseLine l =  (head gestures,last gestures)
     where gestures = map (letterToGesture) $ filter (not . isSpace) l
 
+parseLine2 :: String -> (Gesture,Outcome)
+parseLine2 l =  (letterToGesture $ head trimmed, letterToOutcome $ last trimmed)
+    where trimmed = filter (not . isSpace) l
 
 letterToGesture :: Char -> Gesture
 letterToGesture c 
@@ -50,6 +74,12 @@ letterToGesture c
     | c == 'X' =  Rock
     | c == 'Y' =  Paper
     | c == 'Z' =  Scissors
+
+letterToOutcome :: Char -> Outcome
+letterToOutcome c  
+    | c == 'X' = Loss 
+    | c == 'Y' = Draw 
+    | c == 'Z' = Win
 
 gestureToScore :: Gesture -> Score 
 gestureToScore g = 
@@ -61,8 +91,12 @@ gestureToScore g =
 computeTotalScorePart1 :: [String] -> Score 
 computeTotalScorePart1 = sum . map (computeScorePart1 . parseLine)
 
+computeTotalScorePart2 :: [String] -> Score 
+computeTotalScorePart2 = sum . map (computeScorePart2 . parseLine2)
+
 main :: IO ()
 main = do 
     handle <- openFile "input.txt" ReadMode
     contents <- hGetContents handle
     print $ "Part1: " ++ (show $ computeTotalScorePart1 $ lines $ contents)
+    print $ "Part2: " ++ (show $ computeTotalScorePart2 $ lines $ contents)
