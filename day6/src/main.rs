d24034e947ffa67d8ced4bbb1008ce1c1ded3a75
jmckinny <justinmckinney.md@gmail.com>
2022-12-06 01:53:55 -0500

    cleanup: cargo fmt

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,6 +1,4 @@
-use std::collections::{VecDeque, HashSet};
-
-
+use std::collections::{HashSet, VecDeque};
 
 fn main() {
     let input = std::fs::read_to_string("input.txt").unwrap();
@@ -11,28 +9,27 @@ fn main() {
 fn part1(input: &str) -> usize {
     let mut packet: VecDeque<char> = input.chars().take(4).collect();
 
-    for (i,c) in input.chars().enumerate().skip(4){
+    for (i, c) in input.chars().enumerate().skip(4) {
         packet.pop_front();
         packet.push_back(c);
         let uniq = packet.iter().collect::<HashSet<_>>().len();
-        if uniq == 4{
-            return i+1;
+        if uniq == 4 {
+            return i + 1;
         }
     }
     0
 }
 
-fn part2(input: &str) -> usize{
+fn part2(input: &str) -> usize {
     let mut packet: VecDeque<char> = input.chars().take(14).collect();
 
-    for (i,c) in input.chars().enumerate().skip(14){
+    for (i, c) in input.chars().enumerate().skip(14) {
         packet.pop_front();
         packet.push_back(c);
         let uniq = packet.iter().collect::<HashSet<_>>().len();
-        if uniq == 14{
-            return i+1;
+        if uniq == 14 {
+            return i + 1;
         }
     }
     0
 }
-
